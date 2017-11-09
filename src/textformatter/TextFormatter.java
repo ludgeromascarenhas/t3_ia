892447bed927d776c67658aac394cb5b311cb3fc
Eduardo <eduardo.silveira.006@acad.pucrs.br>
2017-11-09 17:25:19 -0200

    Geração arquivo sem tasg HTML

--- src/textformatter/TextFormatter.java
+++ src/textformatter/TextFormatter.java
@@ -6,11 +6,17 @@
 package textformatter;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
 /**
  *
@@ -22,45 +28,53 @@ public class TextFormatter {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        final String fileName = "esportes.txt";
-        String line = "";
-        StringBuilder texto = new StringBuilder();
-        StringBuilder[] textos;
-
-        int inicia, continua = 0;
+        final String input = "corpus/input/trabalhador.txt";
+        final String output = "corpus/output/trabalhadorOutput.txt";
+        String line;
+        StringBuilder texto;
+        List<StringBuilder> textos = new ArrayList<>();
 
         try {
-            // FileReader reads text files in the default encoding.
-            FileReader fileReader = new FileReader(fileName);
-
-            // Always wrap FileReader in BufferedReader.
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(new FileInputStream(fileName), "Cp1252"));
-            line = bufferedReader.readLine();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input), "Cp1252"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(output, true));
+            
+            line = reader.readLine();
 
             while (line != null) {
                 if (line.startsWith("TEXTO")) {
-                    int numTexto = Integer.parseInt(line.split(" ")[1]);
-                    System.out.println(">> " + numTexto);
+                    writer.write(line);
+                    line = reader.readLine();
+                    texto = new StringBuilder();
 
-                } else {
-
+                    while (!line.equals("</body>")) {
+                        texto.append(" ");
+                        texto.append(line);
+                        line = reader.readLine();
+                    }
+                    texto.append(" ");
                     texto.append(line);
+                    
+                    writer.append("\n");
+                    Document doc = Jsoup.parse(texto.toString());
+                    writer.append(doc.body().text());
+                    writer.append("\n\n");
+                    textos.add(texto);
                 }
 
-                line = bufferedReader.readLine();
+                line = reader.readLine();
             }
 
-            // Always close files.
-            bufferedReader.close();
+            reader.close();
+            writer.close();
+            
         } catch (FileNotFoundException ex) {
             System.out.println(
                     "Unable to open file '"
-                    + fileName + "'");
+                    + input + "'");
         } catch (IOException ex) {
             System.out.println(
                     "Error reading file '"
-                    + fileName + "'");
+                    + input + "'");
         }
     }
-
 }
