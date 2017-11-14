1f26e88dc78b8293005ae8663c783cebd4c123ac
Eduardo <eduardo.silveira.006@acad.pucrs.br>
2017-11-14 13:31:42 -0200

    Arquivos .jar do Cogroo adicionados

--- src/textformatter/TextFormatter.java
+++ src/textformatter/TextFormatter.java
@@ -9,40 +9,41 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+//import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Locale;
+
+import org.cogroo.analyzer.Analyzer;
+import org.cogroo.analyzer.ComponentFactory;
+import org.cogroo.text.Sentence;
+import org.cogroo.text.impl.DocumentImpl;
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
-/**
- *
- * @author 14104887
- */
 public class TextFormatter {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
         final String input = "corpus/input/trabalhador.txt";
-        final String output = "corpus/output/trabalhadorOutput.txt";
+        //final String output = "corpus/output/trabalhadorOutput.txt";
         String line;
         StringBuilder texto;
-        List<StringBuilder> textos = new ArrayList<>();
+
+        ComponentFactory factory = ComponentFactory.create(new Locale("pt", "BR"));
+        Analyzer cogroo = factory.createPipe();
+        
+        DocumentImpl document = new DocumentImpl();
 
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(input), "Cp1252"));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(output, true));
-            
+            //BufferedWriter writer = new BufferedWriter(new FileWriter(output, true));
+
             line = reader.readLine();
 
             while (line != null) {
                 if (line.startsWith("TEXTO")) {
-                    writer.write(line);
+                    //writer.write(line);
                     line = reader.readLine();
                     texto = new StringBuilder();
 
@@ -53,20 +54,26 @@ public class TextFormatter {
                     }
                     texto.append(" ");
                     texto.append(line);
-                    
-                    writer.append("\n");
+
                     Document doc = Jsoup.parse(texto.toString());
-                    writer.append(doc.body().text());
-                    writer.append("\n\n");
-                    textos.add(texto);
+                    //writer.append("\n");
+                    //writer.append(doc.body().text());
+                    //writer.append("\n\n");
+
+                    document.setText(texto.toString());
+
+                    for (Sentence sentence : document.getSentences()) { // lista de sentenças
+                        System.out.println("S: " + sentence.getStart() + " E: " + sentence.getEnd()); // caracteres onde a sentença começa e termina
+                        System.out.println(sentence.getText()); // texto da sentença
+                    }
                 }
 
                 line = reader.readLine();
             }
 
             reader.close();
-            writer.close();
-            
+            //writer.close();
+
         } catch (FileNotFoundException ex) {
             System.out.println(
                     "Unable to open file '"
